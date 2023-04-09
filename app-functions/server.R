83d309b9fd228ae8aacf38f7873216c7f5f7ee00
samanthacsik <samanthacsik@gmail.com>
2023-04-09 10:38:25 -0700

    messy attempt at getting most code on the page (still need for loop for plotting)

--- app-functions/server.R
+++
@@ -1,9 +0,0 @@
-server <- function(input, output) {
-  
-  # render the scatterplot output ----
-  output$penguin_scatterplot <- build_penguin_scatterplot(input)
-  
-  # render the histogram output ----
-  output$penguin_histogram <- build_penguin_histogram(input)
-  
-} # END server
--- app-original/global.R
+++
@@ -1,5 +0,0 @@
-# load packages ----
-library(shiny)
-library(shinyWidgets)
-library(palmerpenguins)
-library(tidyverse)
